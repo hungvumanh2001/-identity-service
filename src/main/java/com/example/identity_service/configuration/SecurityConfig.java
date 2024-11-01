fd6629b7a4fc2d5b1e1627512526c80e2bd0ed53
DESKTOP-9O360Q0\SEATECH <hungvm@seatechit.com.vn>
2024-11-01 17:28:36 +0700

    update code

--- src/main/java/com/example/identity_service/configuration/SecurityConfig.java
+++ src/main/java/com/example/identity_service/configuration/SecurityConfig.java
@@ -1,7 +1,6 @@
 package com.example.identity_service.configuration;
 
 import com.example.identity_service.enums.Role;
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
--- src/main/java/com/example/identity_service/controller/UserController.java
+++ src/main/java/com/example/identity_service/controller/UserController.java
@@ -10,6 +10,7 @@ import jakarta.validation.Valid;
 import lombok.AccessLevel;
 import lombok.RequiredArgsConstructor;
 import lombok.experimental.FieldDefaults;
+import org.springframework.security.access.prepost.PreAuthorize;
 import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
@@ -28,6 +29,7 @@ public class UserController {
         return response;
     }
 
+    @PreAuthorize(value = "hasRole('ADMIN')")
     @GetMapping
     public List<User> getAllUsers() {
         return userService.getAllUsers();
