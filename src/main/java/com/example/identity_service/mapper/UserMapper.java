94ed1853361b4cbd364f6cabd803daff1dc88cb5
DESKTOP-9O360Q0\SEATECH <hungvm@seatechit.com.vn>
2024-11-08 17:25:27 +0700

    update code

--- src/main/java/com/example/identity_service/mapper/UserMapper.java
+++ src/main/java/com/example/identity_service/mapper/UserMapper.java
@@ -4,11 +4,13 @@ import com.example.identity_service.dto.request.UserUpdateRequest;
 import com.example.identity_service.dto.response.UserResponse;
 import com.example.identity_service.entity.User;
 import org.mapstruct.Mapper;
+import org.mapstruct.Mapping;
 import org.mapstruct.MappingTarget;
 
 @Mapper(componentModel = "spring")
 public interface UserMapper {
     User toUser(UserCreationRequest request);
     UserResponse toUserResponse(User user);
+    @Mapping(target = "roles", ignore = true)
     void updateUser(@MappingTarget User user, UserUpdateRequest request);
 }
