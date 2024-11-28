07cac89056f78d46806bf50e9feab3ae2daa04fc
DESKTOP-9O360Q0\SEATECH <hungvm@seatechit.com.vn>
2024-11-28 17:12:04 +0700

    update code

--- src/main/java/com/example/identity_service/configuration/ApplicationInitConfig.java
+++ src/main/java/com/example/identity_service/configuration/ApplicationInitConfig.java
@@ -11,6 +11,7 @@ import lombok.experimental.FieldDefaults;
 import lombok.experimental.NonFinal;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.boot.ApplicationRunner;
+import org.springframework.boot.autoconfigure.condition.ConditionalOnProperty;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.crypto.password.PasswordEncoder;
@@ -32,7 +33,12 @@ public class ApplicationInitConfig {
     static final String ADMIN_PASSWORD = "admin";
 
     @Bean
+    @ConditionalOnProperty(
+            prefix = "spring",
+            value = "datasource.driverClassName",
+            havingValue = "com.mysql.cj.jdbc.Driver")   //dieu kien khi khoi tao bean, de khi UnitTest chay khong khoi tao bean nay len, tranh bi loi
     ApplicationRunner applicationRunner(UserRepository userRepository, RoleRepository roleRepository) {
+        log.info("Init application......");
         return args -> {
             if(userRepository.findByUsername("admin").isEmpty()){
                 roleRepository.save(Role.builder()
--- src/test/java/com/example/identity_service/service/UserServiceTest.java
+++ src/test/java/com/example/identity_service/service/UserServiceTest.java
@@ -11,6 +11,7 @@ import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.boot.test.mock.mockito.MockBean;
+import org.springframework.security.test.context.support.WithMockUser;
 import org.springframework.test.context.TestPropertySource;
 
 import java.time.LocalDate;
